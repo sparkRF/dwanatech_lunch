be3ee59a9f0fb96efa06c6c5e3a93900616f1520
spark-yonsei <144088267+spark-yonsei@users.noreply.github.com>
2023-09-05 21:46:42 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 from PIL import Image
+import pandas as pd
 
 image = Image.open('다운로드.png')
 st.image(image)
@@ -9,5 +10,12 @@ st.write('\n')
 st.write('점심 맛있게 드세요!')
 st.write('\n')
 st.button('점심 추천')
+st.write('\n')
+st.write('\n')
+st.write('전체 메뉴 리스트:')
+st.write(pd.DataFrame({
+    'first column': [1, 2, 3, 4],
+    'second column': [10, 20, 30, 40],
+}))
 st.write('\n')
 st.write('문의사항은 박세일 주임에게 연락주세요.')
