71db3d37dc427f414dfb4bde09ea7754d05175b4
spark-yonsei <144088267+spark-yonsei@users.noreply.github.com>
2023-09-05 21:38:22 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,12 +1,11 @@
 import streamlit as st
 from PIL import Image
 
+image = Image.open('다운로드.png')
+st.image(image)
+
 st.write('동운아나텍 점심식사 추천 페이지입니다.')
 st.write('\n')
 st.write('점심 맛있게 드세요!')
 st.write('\n')
 st.button('점심 추천')
-
-image = Image.open('다운로드.png')
-
-st.image(image)
