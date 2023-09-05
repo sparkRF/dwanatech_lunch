968590266c56690fc58b15f5ea94a8f94c435f11
spark-yonsei <144088267+spark-yonsei@users.noreply.github.com>
2023-09-05 21:31:24 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
 import streamlit as st
 
-st.write('Hello world!')
+public_gsheets_url = https://docs.google.com/spreadsheets/d/1L_4C4UTJ5UGhZC_Afae8Xq7fjcWbmU7R9_QcP02v8HQ/edit?usp=drive_link
+
+st.write('동운아나텍 점심식사 페이지입니다.')
+st.write('\n')
+st.write('점심 맛있게 드세요!')
