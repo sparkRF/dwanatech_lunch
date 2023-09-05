049826de088c6db9767c4b21f5271a5a24f941b0
spark-yonsei <144088267+spark-yonsei@users.noreply.github.com>
2023-09-05 21:57:40 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,8 @@ import streamlit as st
 from PIL import Image
 import pandas as pd
 
+df = pd.read_csv('동운아나텍_점심메뉴.csv', encoding='cp949')
+
 image = Image.open('다운로드.png')
 st.image(image)
 
@@ -18,11 +20,11 @@ if st.button('전체 메뉴 리스트'):
 st.write('\n')
 st.write('문의사항은 박세일 주임에게 연락주세요.')
 
-'''
-df = pd.read_csv('동운아나텍_점심메뉴.csv', encoding='cp949')
+
+
 st.write('전체 메뉴 리스트:')
 st.write(pd.DataFrame({
     '분류': [1,2,3,4],
     '메뉴': [10, 20, 30, 40],
 }))
-'''
+
